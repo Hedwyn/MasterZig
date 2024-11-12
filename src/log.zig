8c08e327aa2a6ca11a8f30fee93d12bb94b11952
Hedwyn <bpestourie@gmail.com>
2024-11-12 14:16:41 +0100

    add missing log module

---
+++ src/log.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+const LoggingLevel = std.log.Level;
+pub const masterzig_log = std.log.scoped(.masterzig);
+
+var log_level = LoggingLevel.debug;
+
+pub fn setLogLevel(level: LoggingLevel) void {
+    log_level = level;
+}
+
+pub fn logFn(
+    comptime level: std.log.Level,
+    comptime scope: @TypeOf(.EnumLiteral),
+    comptime format: []const u8,
+    args: anytype,
+) void {
+    if (@intFromEnum(level) > @intFromEnum(log_level)) {
+        return;
+    }
+    // Ignore all non-error logging from sources other than
+    // .my_project, .nice_library and the default
+    const scope_prefix = "(" ++ switch (scope) {
+        .masterzig, std.log.default_log_scope => @tagName(scope),
+        else => if (@intFromEnum(level) <= @intFromEnum(std.log.Level.err))
+            @tagName(scope)
+        else
+            return,
+    } ++ "): ";
+
+    const prefix = "[" ++ comptime level.asText() ++ "] " ++ scope_prefix;
+    // Print the message to stderr, silently ignoring any errors
+    std.debug.lockStdErr();
+    defer std.debug.unlockStdErr();
+    const stderr = std.io.getStdErr().writer();
+    nosuspend stderr.print(prefix ++ format ++ "\n", args) catch return;
+}
